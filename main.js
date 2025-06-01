040063bce72262b0f4ad3059db0168d9ffff7924
meelyssa2411 <meelyssa2411@gmail.com>
2025-06-01 18:45:51 +0000

    Adição da verificação do preenchimento da input#item

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const items = []
 function addItem() {
     const itemName = document.querySelector("#item").value
 
+    if (itemName === "") {
+        alert("Digite um item válido!")
+        return
+    }
+
     const item = {
         name: itemName,
         checked: false
