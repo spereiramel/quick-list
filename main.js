e9ca084394932f7ceecf4b88834d93d0f711972c
meelyssa2411 <meelyssa2411@gmail.com>
2025-06-01 18:35:13 +0000

    Criação da função checkItem para marcar e desmarcar items do array

--- main.js
+++ main.js
@@ -24,9 +24,11 @@ function showItemsList() {
            <div class="item">
                 <div>
                     <input type="checkbox" name="list" id="item-${index}" ${item.checked && "checked"}>
+
                     <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                         <img src="./assets/assets/checked.svg" alt="checked">
                     </div>
+                    
                     <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
                 </div>
 
